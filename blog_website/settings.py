3337c9c53e816eda3cddda8214e4ed355dc3ccae
priyanshu <priyanshuplpup@gmail.com>
2021-08-14 11:04:41 +0530

    fourth

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'profiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATIC_ROOT = BASE_DIR / 'root_static_files'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+def user_register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f"Account for {username} is created")
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request,'profiles/register.html',{'form' : form})
